f23ba4255dcf40f5cdcb40f60193d7a8d89213f3
M usman ghani <musmanghani0211@gmail.com>
2026-07-14 12:31:12 +0500

    added more tests regarding alert and promt

--- tests/alertsPromtes.spec.js
+++ tests/alertsPromtes.spec.js
@@ -44,7 +44,8 @@ const isvisible = await notfoundtext
             // const table = await page.locator('.oxd-table .orangehrm-employee-list').getByRole('table');
             // const tableData = await table.locator('.oxd-table-body').getByRole('rowgroup');
             const rows = await page.locator('.oxd-table-row');
-            await expect(rows.first()).toBeVisible();
+    
+            await expect(rows).toHaveCount(2);
             const selectedRow = await rows.locator('.oxd-table-cell');
             const selectedRowButtons = await selectedRow.locator('.oxd-table-cell-actions');
             const deletebuttonForSelectedRow = await selectedRowButtons.locator('.oxd-icon-button').last();
@@ -57,9 +58,99 @@ const isvisible = await notfoundtext
     }
 })
 
+//promt of the web page elemenets handling
+test('alert handle', async({page}) => {
+    await page.goto('https://the-internet.herokuapp.com/javascript_alerts');
+    
+    page.on('dialog', async dialog => {
+        // console.log('dialog type', dialog.type());
+        // console.log('dialog text', dialog.message());
+        await dialog.accept();
+    })
+
+    await page.getByRole('button', {name: 'Click For JS Alert'}).click();
+    await expect(page.locator('#result')).toContainText('You successfully clicked an alert');
+
+    await page.getByRole('button', {name: 'Click for JS Confirm'}).click();
+    await expect(page.locator('#result')).toContainText('You clicked: Ok');
+})
+
+test('Handle confirm - with OK', async({page}) => {
+    await page.goto('https://the-internet.herokuapp.com/javascript_alerts');
+    
+    page.on('dialog', async dialog => {
+        // console.log('dialog type', dialog.type());
+        // console.log('dialog text', dialog.message());
+        await dialog.accept();
+    })
+
+    await page.getByRole('button', {name: 'Click for JS Confirm'}).click();
+    await expect(page.locator('#result')).toContainText('You clicked: Ok');
+})
 
 
+test('Handle confirm - with dismiss', async({page}) => {
+    await page.goto('https://the-internet.herokuapp.com/javascript_alerts');
+    
+    page.on('dialog', async dialog => {
+        // console.log('dialog type', dialog.type());
+        // console.log('dialog text', dialog.message());
+        await dialog.dismiss();
+    })
+
+    await page.getByRole('button', {name: 'Click for JS Confirm'}).click();
+    await expect(page.locator('#result')).toContainText('You clicked: Cancel');
+})
+
+test('Handle promt box with adding values and accept', async({page}) => {
+    await page.goto('https://the-internet.herokuapp.com/javascript_alerts');
+    
+    page.on('dialog', async dialog => {
+        // console.log('dialog type', dialog.type());
+        // console.log('dialog text', dialog.message());
+        // let Username = ''
+        await dialog.accept('Usman Ghani');
+    })
+
+    await page.getByRole('button', {name: 'Click for JS Prompt'}).click();
+    await expect(page.locator('#result')).toContainText(`You entered: Usman Ghani `);
+})
 
 
+test('iFrame containing', async ({page}) => {
+    await page.goto('https://the-internet.herokuapp.com/iframe');
+    const tinymceContainer = page.locator('.tox-editor-container');
+    const iframe = page.frameLocator('iframe[title="Rich Text Area"]');
+    const Textintothebox = await iframe.locator('#tinymce');
+    const toxNotificationContainer = page.locator('.tox-notifications-container');
+    const toxNotification = toxNotificationContainer.locator('.tox-notification');
 
+    const toxNotificationdismiss = toxNotification.locator('.tox-notification__dismiss');
+    await toxNotificationdismiss.click();
+    await expect(toxNotificationContainer).not.toBeVisible();
+    // console.log(await Textintothebox.allTextContents());
+    await expect(Textintothebox).toContainText('Your content goes here.');
 
+})
+
+//nested framing
+
+test('nested framing', async({page}) => {
+    await page.goto('https://the-internet.herokuapp.com/nested_frames');
+    const topFrame = page.frameLocator('frame[name="frame-top"]');
+    // const middleFrame = await topFrame.frameLocator('iframe[title= "frameset-middle"]');
+    const leftFrameOftopframe =  topFrame.frameLocator('frame[name="frame-left"]');
+    const middleFrameOftopframe =  topFrame.frameLocator('frame[name="frame-middle"]');
+    const rightFrameOftopframe =  topFrame.frameLocator('frame[name="frame-right"]');
+    const bottomFram = page.frameLocator('frame[name="frame-bottom"]');
+
+    const TextofleftFrameOftopframe = (await leftFrameOftopframe.locator('body').allTextContents())[0].trim();
+    console.log("Left frame text is: ", TextofleftFrameOftopframe)
+    const TextofmiddleFrameOftopframe = (await middleFrameOftopframe.locator('body').allTextContents())[0].trim();
+    console.log("Middle frame text is: ",TextofmiddleFrameOftopframe);
+    const textofrightFrameOftopframe = (await rightFrameOftopframe.locator('body').allTextContents())[0].trim();
+    console.log("Right frame text is: ",textofrightFrameOftopframe);
+    const textofbottomframe = (await bottomFram.locator('body').allTextContents())[0].trim();
+    console.log("Bottom frame text is: ",textofbottomframe)
+
+})
